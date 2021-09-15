45cbd92803f853d4da4f8072a1da1c68723532e0
Diogo <diogofilipebotelho@gmail.com>
2021-09-15 15:38:55 -0400

    Created User class, base.html, users.html and new-user.html

---
+++ app.py
@@ -0,0 +1,16 @@
+"""Blogly application."""
+
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    '''Connect to database.'''
+
+    db.app = app
+    db.init_app(app)
+
+"""Models for Blogly."""
+
+class User(db.Model):
+    '''User.'''
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(30),
+                           nullable=False)
+    last_name = db.Column(db.String(30),
+                          nullable=False)
+    image_url = db.Column(Text,
+                          nullable=True,
+                          default="https://anthonycarbonepersonalinjurylawyer.com/wp-content/uploads/2018/06/shutterstock_126920099.jpg")
+
