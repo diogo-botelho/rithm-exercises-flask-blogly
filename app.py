d2b7dea6062dd332fdb3182732d0ed15d3567586
Diogo <diogofilipebotelho@gmail.com>
2021-09-15 19:40:15 -0400

    Fixed a bug with our new-user.html form; Added show_new_user_form(), add_user() and show_user_details() in app.py

--- app.py
+++ app.py
@@ -32,3 +32,33 @@ def show_users():
 
     return render_template("users.html", 
                             users = users)
+
+@app.get("/users/new")
+def show_new_user_form():
+    """Show form new user"""
+
+    return render_template("new-user.html")
+
+@app.post("/users/new")
+def add_user():
+    """Add new user"""
+    breakpoint()
+    response = request.form
+    breakpoint()
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+
+    breakpoint()
+    new_user = User(first_name=first_name,last_name=last_name,image_url=image_url)
+    breakpoint()
+    db.session.add(new_user)
+    db.session.commit()
+    breakpoint()
+    return redirect("/users")
+
+@app.get("/users/[user-id]")
+def show_user_details():
+    """Show details for selected user"""
+
+    return render_template("/user-detail.html")
