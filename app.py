29cdb010b616554589c0cd20ba81e2a365676a82
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-09-16 17:26:32 -0700

    adding tests for post add and delete, and code review tweaks

--- app.py
+++ app.py
@@ -18,20 +18,23 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
+
 @app.get("/")
 def redirect_to_users():
     """Redirect to Users page"""
 
     return redirect("/users")
 
+
 @app.get("/users")
 def show_users():
     """Show a list of all users"""
 
-    users = User.query.all() 
+    users = User.query.all()
 
-    return render_template("users.html", 
-                            users = users)
+    return render_template("users.html",
+                           users=users)
+
 
 @app.get("/users/new")
 def show_new_user_form():
@@ -39,39 +42,45 @@ def show_new_user_form():
 
     return render_template("new-user.html")
 
+
 @app.post("/users/new")
 def add_user():
     """Add new user"""
 
     first_name = request.form["first-name"]
     last_name = request.form["last-name"]
-    image_url = request.form["image-url"] or None #Code review: Without the "or None" it was passing an empty string and the model needs a Null to put the template image
+    # Code review: Without the "or None" it was passing an empty string and the model needs a Null to put the template image
+    image_url = request.form["image-url"] or None
 
     new_user = User(
-                    first_name=first_name,
-                    last_name=last_name,
-                    image_url=image_url
-                    )
-    
+        first_name=first_name,
+        last_name=last_name,
+        image_url=image_url
+    )
+
     db.session.add(new_user)
     db.session.commit()
 
     flash(f"User {first_name} {last_name} successfully added.")
-    
+
     return redirect("/users")
+
 
 @app.get("/users/<int:user_id>")
 def show_user_details(user_id):
     """Show details for selected user"""
 
-    user = User.query.get_or_404(user_id) #Code Review: .get returns the page with all the values as "None" but still showing the template HTML. Also one click away from a bug if users try to delete the non-existing user
-
+    # Code Review: .get returns the page with all the values as "None" but still showing the template HTML. Also one click away from a bug if users try to delete the non-existing user
+    user = User.query.get_or_404(user_id)
+    
     posts = user.posts
+    # posts = Post.query.filter(user_id == user.id)
 
     return render_template(
-                            "/user-detail.html",
-                            user= user,
-                            posts= posts)
+        "/user-detail.html",
+        user=user,
+        posts=posts)
+
 
 @app.get("/users/<int:user_id>/edit")
 def show_user_edit_form(user_id):
@@ -81,11 +90,12 @@ def show_user_edit_form(user_id):
 
     return render_template("edit-user.html", user=user)
 
+
 @app.post("/users/<int:user_id>/edit")
 def edit_user(user_id):
     """Process the edit form, returning the user to the /users page."""
 
-    edited_user = User.query.get_or_404(user_id) #Code Review
+    edited_user = User.query.get_or_404(user_id)  # Code Review
 
     edited_user.first_name = request.form["first-name"]
     edited_user.last_name = request.form["last-name"]
@@ -97,18 +107,20 @@ def edit_user(user_id):
 
     return redirect("/users")
 
+
 @app.post("/users/<int:user_id>/delete")
 def delete_user(user_id):
     """Delete the user."""
 
     user = User.query.get(user_id)
-    db.session.delete(user) #user.query.get.delete also a good alternative
+    db.session.delete(user)  # user.query.get.delete also a good alternative
     db.session.commit()
 
     flash("User successfully deleted.")
 
     return redirect("/users")
 
+####################################POSTS ROUTES
 @app.get("/users/<int:user_id>/posts/new")
 def show_add_post_form(user_id):
     """Show form to add a post for that user."""
@@ -117,35 +129,35 @@ def show_add_post_form(user_id):
 
     return render_template("new-post.html", user=user)
 
+
 @app.post("/users/<int:user_id>/posts/new")
 def add_post(user_id):
     """Handle add form; add post and redirect to the user detail page"""
-
+    
+    user = User.query.get_or_404(user_id)
     title = request.form["title"]
     content = request.form["content"]
 
-    new_post = Post(title=title, content=content, user_id=user_id)
+    user.posts.append(Post(title=title, content=content))
 
-    db.session.add(new_post)
     db.session.commit()
 
-    flash(f"'{new_post.title}' was successfully added.")
-    
-    return redirect(f"/users/{user_id}")
+    flash(f"'{title}' was successfully added.")
+
+    return redirect(f"/users/{user.id}")
+
 
 @app.get("/posts/<int:post_id>")
 def show_post(post_id):
     """Show post."""
 
     post = Post.query.get_or_404(post_id)
-    
-    user = post.user
 
     return render_template(
-                            "post-detail.html",
-                            post = post,
-                            user = user
-                            )
+        "post-detail.html",
+        post=post
+    )
+
 
 @app.get("/posts/<int:post_id>/edit")
 def show_edit_post_form(post_id):
@@ -153,19 +165,17 @@ def show_edit_post_form(post_id):
 
     post = Post.query.get_or_404(post_id)
 
-    user = post.user
-
     return render_template(
-                            "edit-post.html",
-                            post = post,
-                            user = user
-                            )
+        "edit-post.html",
+        post=post
+    )
+
 
 @app.post("/posts/<int:post_id>/edit")
 def edit_post(post_id):
     """Edit post."""
 
-    edited_post = Post.query.get_or_404(post_id) #Code Review
+    edited_post = Post.query.get_or_404(post_id)  
 
     edited_post.title = request.form["title"]
     edited_post.content = request.form["content"]
@@ -176,15 +186,16 @@ def edit_post(post_id):
 
     return redirect(f"/posts/{post_id}")
 
+
 @app.post("/posts/<int:post_id>/delete")
 def delete_post(post_id):
     """Delete post."""
 
-    post = Post.query.get(post_id)
-    user = post.user
+    post = Post.query.get_or_404(post_id)
+
     db.session.delete(post)
     db.session.commit()
 
     flash("Post successfully deleted.")
 
-    return redirect(f"/users/{user.id}")
+    return redirect(f"/users/{post.user_id}")
--- test_app.py
+++ test_app.py
@@ -1,20 +1,17 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
-# Use test database and don't clutter tests with SQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
-# Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
-
-# This is a bit of hack, but don't use Flask DebugToolbar
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 db.drop_all()
 db.create_all()
+
 
 class BloglyViewsTestCase(TestCase):
     """Tests for views for Blogly"""
@@ -22,6 +19,7 @@ class BloglyViewsTestCase(TestCase):
     def setUp(self):
         """Add sample user."""
 
+        Post.query.delete()
         User.query.delete()
 
         user = User(first_name="TestFirstName",
@@ -33,29 +31,33 @@ class BloglyViewsTestCase(TestCase):
 
         self.user_id = user.id
 
+        post = Post(title="Seeded Title",
+                    content="Seeded content",
+                    user_id=self.user_id
+                    )
+        db.session.add(post)
+        db.session.commit()
+
+        self.post_id = post.id
+
     def tearDown(self):
         """Clean up any fouled transaction."""
 
         db.session.rollback()
 
-    def test_redirect_to_users(self):
-        with app.test_client() as client:
-            resp = client.get("/")
-            html = resp.get_data(as_text=True)
-
-            self.assertEqual(resp.status_code, 302)
-            self.assertEqual(resp.location, "http://localhost/users")
-
     def test_redirect_to_users_followed(self):
+        """Test that redirect from / works"""
+        
         with app.test_client() as client:
-            resp = client.get("/",follow_redirects=True)
+            resp = client.get("/", follow_redirects=True)
             html = resp.get_data(as_text=True)
 
-            self.assertEqual(resp.status_code,200)
+            self.assertEqual(resp.status_code, 200)
             self.assertIn('<h1>Users</h1>', html)
 
-
     def test_show_users(self):
+        """Test that /users route works """
+        
         with app.test_client() as client:
             resp = client.get("/users")
             html = resp.get_data(as_text=True)
@@ -64,24 +66,57 @@ class BloglyViewsTestCase(TestCase):
             self.assertIn('TestFirstName', html)
 
     def test_add_user(self):
+        """Test that user is added."""
+        
         with app.test_client() as client:
             test_new_user = {
                 "first-name": "TestFirstName2",
                 "last-name": "TestLastName2",
                 "image-url": "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-                }
-            resp = client.post("/users/new", 
-                                data=test_new_user,
-                                follow_redirects=True)
+            }
+            resp = client.post("/users/new",
+                               data=test_new_user,
+                               follow_redirects=True)
             html = resp.get_data(as_text=True)
-            self.assertEqual(resp.status_code,200)
+            self.assertEqual(resp.status_code, 200)
             self.assertIn("<h1>Users</h1>", html)
             self.assertIn("TestFirstName2", html)
-    
+
     def test_show_user_edit_form(self):
+        """Test that user edit form works"""
+        
         with app.test_client() as client:
             resp = client.get(f"/users/{self.user_id}/edit")
             html = resp.get_data(as_text=True)
 
-            self.assertEqual(resp.status_code,200)
-            self.assertIn("<h1>Edit a user</h1>", html)       
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<h1>Edit a user</h1>", html)
+
+    def test_add_post(self):
+        """Test adding a post"""
+
+        with app.test_client() as client:
+            new_post = {
+                "title": "Test title",
+                "content": "Post content here"
+            }
+            resp = client.post(f"/users/{self.user_id}/posts/new",
+                               data=new_post,
+                               follow_redirects=True
+                               )
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Test title", html)
+
+    def test_delete_post(self):
+        """Test deleting a post."""
+
+        with app.test_client() as client:
+            resp = client.post(f"/posts/{self.post_id}/delete",
+                               follow_redirects=True
+                               )
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn("Seeded Title", html)
